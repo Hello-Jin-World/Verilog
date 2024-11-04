1e75a2c53bdc1d61cb234ec762991e2e6dabf8ad
Hello-Jin-World <daniel45598190@gmail.com>
2024-11-04 11:40:18 +0900

    test

---
+++ project_1/project_1.srcs/sources_1/new/gates.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/11/04 11:20:34
+// Design Name: 
+// Module Name: gates
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module gates(
+    input a,
+    input b,
+    output y0,
+    output y1,
+    output y2,
+    output y3,
+    output y4,
+    output y5,
+    output y6
+    );
+    
+    assign y0 = a & b; // AND OPERATION
+    assign y1 = ~(a & b); // NAND OPERATION
+    assign y2 = a | b; // OR OPERATION
+    assign y3 = ~(a | b); // NOR OPERATION
+    assign y4 = a ^ b; // XOR OPERATION
+    assign y5 = ~(a ^ b); // NOTXOR OPERATION
+    assign y6 = ~a;
+endmodule
---
+++ project_1/project_1.srcs/sources_1/new/sim_1.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/11/04 11:17:10
+// Design Name: 
+// Module Name: sim_1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sim_1(
+
+    );
+endmodule
