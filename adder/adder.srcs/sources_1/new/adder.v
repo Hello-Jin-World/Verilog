20712954b9c61f250f7f26aeea98561f5d0b36a3
Hello-Jin-World <daniel45598190@gmail.com>
2024-11-04 16:28:57 +0900

    full

--- adder/adder.srcs/sources_1/new/adder.v
+++ adder/adder.srcs/sources_1/new/adder.v
@@ -19,6 +19,34 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+module full_adder(
+    input a,
+    input b,
+    input cin,
+    output sum,
+    output carry
+);
+    wire sum1, carry1, sum2, carry2;
+    
+half_adder U_HA1( // 1st Half Adder
+    .a(a),
+    .b(b),
+    .sum(sum1),
+    .carry(carry1)
+);
+
+half_adder U_HA2( // 2nd Half Adder
+    .a(sum1), 
+    .b(cin),
+    .sum(sum2),
+    .carry(carry2)
+);
+
+    assign carry = carry1 | carry2;
+    assign sum = sum2;
+
+endmodule
+
 
 module half_adder(
     input a,
