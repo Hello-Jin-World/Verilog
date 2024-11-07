14028ec90198e4123e00310b72f0d3eb6dda5065
Hello-Jin-World <daniel45598190@gmail.com>
2024-11-07 15:38:31 +0900

    lector_fsm_led

---
+++ 1107_FSM_led_onoff/1107_FSM_led_onoff.srcs/sources_1/new/fsm_led.v
@@ -0,0 +1,64 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/11/07 15:27:50
+// Design Name: 
+// Module Name: fsm_led
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fsm_led (
+    input  clk,
+    input  reset,
+    input  switch,
+    output led
+);
+
+    reg state, state_next;
+
+    //state registor
+    always @(posedge clk, posedge reset) begin
+        if (reset) begin
+            state <= 1'b0;
+        end else begin
+            state <= state_next;
+        end
+    end
+
+    // next state combinational logic
+    always @(*) begin  // detect all input
+        case (state)
+            1'b0: begin
+                if (switch == 1'b1) begin
+                    state_next = 1'b1;
+                end
+                else begin
+                    state_next = 1'b0;
+                end
+            end
+            1'b1: begin
+                if (switch == 1'b0) begin
+                    state_next = 1'b0;
+                end
+                else begin
+                    state_next = 1'b1;
+                end
+            end
+            default: state_next = state;
+        endcase
+    end
+
+endmodule
