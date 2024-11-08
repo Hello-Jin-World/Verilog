df4e795ca239c12c9faca2b2d3bb6b351f38ca6f
Hello-Jin-World <daniel45598190@gmail.com>
2024-11-08 10:01:37 +0900

    mealy

---
+++ 1108_FSM_led_Mealy/1108_FSM_led_Mealy.srcs/sources_1/new/led_Mealy.v
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/11/08 09:54:06
+// Design Name: 
+// Module Name: led_Mealy
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module led_Mealy (
+    input  clk,
+    input  reset,
+    input  switch,
+    output led
+);
+    parameter LED_OFF = 1'b0, LED_ON = 1'b1;
+    reg state, state_next;
+
+    // state register
+    always @(posedge clk, posedge reset) begin
+        if (reset) begin
+            state <= LED_OFF;
+        end else begin
+            state <= state_next;
+        end
+    end
+
+    // next state combinational logic
+    always @(*) begin
+        state_next = state; // default
+        case (state)
+            LED_OFF : begin
+                if (switch == 1'b1) state_next = LED_ON;
+            end
+            LED_ON : begin
+                if (switch == 1'b0) state_next = LED_OFF;
+            end
+        endcase
+    end
+endmodule
