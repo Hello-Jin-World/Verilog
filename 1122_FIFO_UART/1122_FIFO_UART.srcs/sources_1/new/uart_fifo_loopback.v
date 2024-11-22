e1b4828ced43a6f32090dd61fae2fad88603a85b
Hello-Jin-World <daniel45598190@gmail.com>
2024-11-22 10:32:09 +0900

    debugging_ip

---
+++ 1122_FIFO_UART/1122_FIFO_UART.srcs/sources_1/new/uart_fifo_loopback.v
@@ -0,0 +1,86 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/11/22 09:44:59
+// Design Name: 
+// Module Name: uart_fifo_loopback
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module uart_fifo_loopback (
+    input  clk,
+    input  reset,
+    input  rx,
+    output tx
+);
+
+    wire       tx_start;
+    wire [7:0] loop_data0;
+    wire [7:0] loop_data1;
+    wire [7:0] loop_data2;
+    wire       tx_busy;
+    wire       rx_done;
+    wire       rx_fifo_empty;
+    wire       tx_fifo_full;
+
+    ila_0 your_instance_name (
+        .clk(clk),  // input wire clk
+
+
+        .probe0(tx),  // input wire [0:0]  tx
+        .probe1(rx),  // input wire [0:0]  rx
+        .probe2(tx_start),  // input wire [0:0] tx_start 
+        .probe3(rx_done),  // input wire [0:0]  rx_done
+        .probe4(loop_data2),  // input wire [7:0]  tx_data
+        .probe5(loop_data0)  // input wire [7:0] rx_data 
+    );
+
+    uart U_uart (
+        .clk(clk),
+        .reset(reset),
+        .tx_start(~tx_start),
+        .tx_data(loop_data2),
+        .tx(tx),
+        .tx_busy(tx_busy),
+        .tx_done(),
+        .rx(rx),
+        .rx_data(loop_data0),
+        .rx_done(rx_done)
+    );
+
+
+    fifo U_TX_fifo (
+        .clk  (clk),
+        .reset(reset),
+        .wdata(loop_data1),
+        .wr_en(~rx_fifo_empty),
+        .rd_en(tx_busy),
+        .rdata(loop_data2),
+        .full (tx_fifo_full),
+        .empty(tx_start)
+    );
+
+    fifo U_RX_fifo (
+        .clk  (clk),
+        .reset(reset),
+        .wdata(loop_data0),
+        .wr_en(rx_done),
+        .rd_en(~tx_fifo_full),
+        .rdata(loop_data1),
+        .full (),
+        .empty(rx_fifo_empty)
+    );
+endmodule
